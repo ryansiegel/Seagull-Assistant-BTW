6fff051c3b8cc0b8eefc5736bc75f7491a2579d7
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2022-03-20 23:36:17 -0400

    Update btwbot.py

--- bot/btwbot.py
+++ bot/btwbot.py
@@ -8,9 +8,26 @@ from github import Github
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', description="This is a Helper Bot", intents=intents)
 
+'''
+CHANNEL ACCESS LIST
+'''
+#save IDs of channels
+bot.staffChannel = 0
+
+'''
+BOT START UP
+'''
 @bot.event
 async def on_ready():
-  print('ready')
+  #grab channels
+	await grabChannelID()
+  await bot.staffChannel.send('Hey there fuckers. It\'s me, your bot. I am online and ready to fuck shit up!')
+  
+'''
+CHANNEL ACCESS LIST - grab channel IDs from Heroku env list and assign it to vars
+'''
+async def grabChannelID():
+	bot.staffChannel = bot.get_channel(int(os.getenv("STAFF_CHANNEL_ID")))
   
   
 #token to run bot
