e5b034a2b761781d8000647f888ec80e95a262e0
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2022-03-21 02:52:56 -0400

    Update btwbot.py

--- bot/btwbot.py
+++ bot/btwbot.py
@@ -35,17 +35,43 @@ async def on_message(message):
 		if message.author == bot.user:
 			return
 		'''
-		STICKY MESSAGE - creates a 'pinned' message at the bottom of the intro channel. Basically deletes the old message (Saved to bot.stickymessage) and
+		STICKY MESSAGE FOR PVP CHALLENGES - creates a 'pinned' message at the bottom of the pvp challenges channel. Basically deletes the old message (Saved to bot.stickymessage) and
 				posts a new one whenever anyone comments.
 		'''
+		#>>>>>>>>>>Collect current message ID
+		#connect to repo
+		g = Github(os.getenv("GITHUB_TOKEN"))
+		repo = g.get_repo(os.getenv("GITHUB_REPO"))
+		#grab contents from repo
+		getStickyMessageGitHub = repo.get_contents("bot/PvPChallengesStickyMessageID.txt")
+		#grab full content from file
+		decodedStickyMessageGitHub = getStickyMessageGitHub.decoded_content.decode()
+		#define vars
+		stickyMessageIDs = []
+		#put contents in list
+		stickyMessageIDs = decodedStickyMessageGitHub.split("\n")
+		#remove empty space at beginning of each string in list
+		stickyMessageIDs = [item.lstrip() for item in stickyMessageIDs]
+		stickyMessageIDs = list(filter(None, stickyMessageIDs))
+		#>>>>>>>>>>Delete old message and post new message
 		#delete previous message, otherwise alert that there is no alert to delete
-		try:
-			await bot.stickyMessagePvPChallenges.delete()
-		except:
-			print('nothing to delete')
-		#post new message at bottom of intro channel
+		for x in stickyMessageIDs:
+			try:
+				await bot.stickyMessageIDs[i].delete()
+			except:
+				print('nothing to delete')
+		#post new message at bottom of PvP Challenges channel
 		embedVar = discord.Embed(title="Current Active Challenges:", description="**__01. It Takes Two__** *[Difficulty: Hard]*\nTo complete this challenge, you must win a battle against your opponent using 2 Pokemon (CP can be any range as long as it is eligible in the cup). The third Pokemon can be any Pokemon eligible in the cup, however it must be at 10 CP. The 10 CP Pokemon is allowed to be used in battle as well.\n\n**__02. Get Beamed__** *[Difficulty: Easy]*\nTo complete this challenge, you must successfully land a Solar Beam or Hyper Beam against an opponent and knock out the Pokemon.\n\n**__03. Overpowered__** *[Difficulty: Medium]*\nTo complete this challenge, you must win a battle against your opponent. However, all 3 Pokemon on your team must be alive (as long as it is above 0 HP) and both shields are still available (meaning unused).\n\n**__04. Community Battler__** *[Difficulty: Easy]*\nDuring community day, you must win a battle against an opponent using the Pokemon featured & its event move during the Pokemon GO Community Day.\n\n**__05. Student to Master__** *[Difficulty: Depends on Staff]*\nDefeat in a battle any BTW staff during a stream or competition.", color=0x00FF00)
-		bot.stickyMessagePvPChallenges = await bot.pvpChallengesChannel.send(embed=embedVar)
+		stickyMessage1 = await bot.pvpChallengesChannel.send(embed=embedVar)
+		#>>>>>>>>>>Save new message ID
+		stickyMessageIDs, stickyMessageIDsStr = [], ""
+		stickyMessageIDs.append(str(stickyMessage1).lstrip())
+		#add \n to each item in list to put each item on new line in file
+		stickyMessageIDs = [item + "\n" for item in stickyMessageIDs]
+		#transform list to string
+		stickyMessageIDsStr = "".join(stickyMessageIDs)
+		#update files in repo
+		repo.update_file("bot/PvPChallengesStickyMessageID.txt", "Updated", stickyMessageIDsStr, getStickyMessageGitHub.sha)
 	
 	
 	
